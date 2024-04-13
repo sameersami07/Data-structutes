eab640a11871ebc0562e6d10959b638a81188ddd
sameersami07 <166928196+sameersami07@users.noreply.github.com>
2024-04-13 21:47:08 +0530

    insertionsort.c

---
+++ insertion.c
@@ -0,0 +1,26 @@
+// insertion sort
+#include<stdio.h>
+void insertionsort(int [ ], int);
+int main() {
+    int i,a[30],n;
+    printf("Enter no. of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements:\n");
+    for(i=0; i<n; i++)
+        scanf("%d",&a[i]);
+    insertionsort(a,n);
+    printf("After sorting:\n");
+    for(i=0; i<n; i++)
+        printf("%d ",a[i]);
+}
+
+void insertionsort(int a[ ], int n) {
+    int i, j, item;
+    for(i=1; i<n; i++)
+    {
+        item = a[i];
+        for(j=i-1; j>=0 && a[j]>item; j--)
+            a[j+1] = a[j];
+        a[j+1] = item;
+    }
+}
